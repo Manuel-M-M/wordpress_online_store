b21be1b430687a67e3b5bb63d35948289832d243
Manuel-M-M <mmoragamolina@gmail.com>
2021-06-07 09:56:41 +0200

    I modify a filter by adding a function and removing the phone field from the fields that the filter receives. I finally create a new field.

--- wp-content/themes/v-game_store_child/functions.php
+++ wp-content/themes/v-game_store_child/functions.php
@@ -6,3 +6,20 @@ add_action('woocommerce_review_before', 'game_store_action_avatar', 10);
 function game_store_action_avatar() {
     echo "<i class='fas fa-user-astronaut'></i>";
 }
+
+add_filter('woocommerce_checkout_fields', 'game_store_filter_fields', 20);
+
+function game_store_filter_fields($fields) {
+    
+    // Remove field phone from form
+    unset($fields['billing']['billing_phone']);
+
+    // Add new field to form
+    $fields['billing']['new_field'] = [
+        'type' => 'text',
+        'label' => 'Campo nuevo',
+        'placeholder' => 'introduce tu campo nuevo'
+    ];
+
+    return $fields;
+}
